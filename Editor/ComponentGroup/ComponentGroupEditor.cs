680cbc355b1c87f6ff4bede1bfd2453dfe0f7589
bittinkerer <bittinkerer@gmail.com>
2023-10-28 09:35:09 -0700

    Binding progress

--- Editor/ComponentGroup/ComponentGroupEditor.cs
+++ Editor/ComponentGroup/ComponentGroupEditor.cs
@@ -16,27 +16,37 @@ namespace Packages.Estenis.ComponentGroupsEditor_
         [SerializeField] private VisualTreeAsset _editorAsset;
         [SerializeField] private VisualTreeAsset _componentAsset;
 
+        public ComponentGroup Target => target as ComponentGroup;
+
         private TemplateContainer _componentTemplate;
 
         public override VisualElement CreateInspectorGUI()
         {
-            var root = _editorAsset.CloneTree();
-            _componentTemplate = _componentAsset.CloneTree();
-
-            var componentObjectField = _componentTemplate.Q<ObjectField>();
-            componentObjectField.RegisterValueChangedCallback(OnComponentChanged);
+            var root = _editorAsset.Instantiate();
 
             // Set up groups
             var groupsListView = root.Q<ListView>();
+            groupsListView.itemsSource = Target._components;
             //groupsListView.makeItem = _componentAsset.CloneTree;
             groupsListView.makeItem = () =>
             {
                 var tc = _componentAsset.Instantiate();
+                var componentField = tc.Q<ObjectField>();
+                // componentField.value is Component type and binds to _component of a ComponentData type
+                Target._components[Target._components.Count - 1] = (componentField.value as SerializedProperty);
+
                 tc.Q<ObjectField>().RegisterValueChangedCallback(evt =>
                 {
+                    Debug.Log($"[{Time.time}] Component Changed..");
+                    // hide assigned component
                     var component = (Component)evt.newValue;
-                    component.HideInInspector();
-                    Debug.Log("Is this working or what..");
+                    componentField.value = component;
+                    if (component != null)
+                    {
+                        component.HideInInspector();
+                    }
+                    EditorUtility.SetDirty(target);
+
                 });
                 return tc;
             };
@@ -58,50 +68,49 @@ namespace Packages.Estenis.ComponentGroupsEditor_
 
         private void OnComponentChanged(ChangeEvent<UnityEngine.Object> evt)
         {
-            Debug.Log($"{nameof(OnComponentChanged)}");
+            Debug.Log($"[{Time.time}] {nameof(OnComponentChanged)}");
         }
 
         private void GroupsListView_itemsSourceChanged()
         {
-            Debug.Log("Items Source changed.");
+            Debug.Log("[{Time.time}] Items Source changed.");
         }
 
         private void OnComponentsChanged(SerializedProperty property)
         {
-            Debug.Log("Components changed.");
+            Debug.Log($"[{Time.time}] Components changed.");
         }
 
         private void GroupsListView_itemIndexChanged(int arg1, int arg2)
         {
-            Debug.Log("Item index changed");
+            Debug.Log($"[{Time.time}] Item index changed");
         }
 
         private void GroupsListView_itemsAdded(System.Collections.Generic.IEnumerable<int> addedItems)
         {
-            Debug.Log($"Groups item added:  {string.Join(',', addedItems.ToArray())}");
-            _componentTemplate.Query<ObjectField>()
-                .ForEach(of =>
-                {
-                    of.RegisterValueChangedCallback(OnComponentChanged);
-                    of.RegisterCallback<ChangeEvent<ObjectField>>(OnComponentAChange);
-                });
-            var foo = _componentTemplate.Query<ObjectField>();
+            Debug.Log($"[{Time.time}] Groups item added:  {string.Join(',', addedItems.ToArray())}");
+            //_componentTemplate.Query<ObjectField>()
+            //    .ForEach(of =>
+            //    {
+            //        of.RegisterValueChangedCallback(OnComponentChanged);
+            //        of.RegisterCallback<ChangeEvent<ObjectField>>(OnComponentAChange);
+            //    });
+            //var foo = _componentTemplate.Query<ObjectField>();
             //last.RegisterValueChangedCallback(OnComponentChanged);
         }
 
-        private void OnComponentAChange(ChangeEvent<ObjectField> evt)
-        {
-            Debug.Log("COMOI");
-        }
-
+        //
+        // Will bind to UI from backend data
         private void OnGroupsItemBound(VisualElement element, int index)
         {
-            Debug.Log("Groups item bound");
+            Debug.Log($"[{Time.time}] Groups item bound");
+            var componentField = element.Q<ObjectField>();
+            componentField.value = Target._components[index]._component;
         }
 
         private void OnGroupsItemUnBound(VisualElement element, int index)
         {
-            Debug.Log("Groups item unbound");
+            Debug.Log($"[{Time.time}] Groups item unbound");
         }
 
     }
